6426b67d2bab99e31b3ba5312453896efe8dc686
statwonk <cpeter9@gmail.com>
2014-07-20 12:43:06 -0400

    Nested route type, route and lane selection.

--- site/server.R
+++ site/server.R
@@ -1,5 +1,14 @@
 library(shiny)
 library(eotr)
+
+show_selection_based_on <- function(input_selection, item_to_test) {
+  if (grepl("All ", input_selection)) {
+    return(rep(TRUE, length(item_to_test)))
+  } else {
+    return(item_to_test %in% input_selection)
+  }
+}
+
 shinyServer(function(session, input, output) {
   load("site_data.rda")
 
--- site/view.R
+++ site/view.R
@@ -1,46 +1,54 @@
-show_selection_based_on <- function(input_selection, item_to_test) {
-  ifelse(grepl("All ", input_selection),
-         rep(TRUE, length(item_to_test)),
-         item_to_test %in% input_selection)
-}
-
 output$route_type_ui <- renderUI({
   selectInput("route_type",
               "Choose route type: ",
-              choices = c("All route types", sort(unique(munged_data$route_type))),
-              selected = "All route types")
-}); selected_route_type <- reactive({ input$route_type })
+              choices = c("All route types",
+                          sort(unique(munged_data$route_type
+                          ))))
+})
 
 output$route_ui <- renderUI({
   selectInput("route",
               "Choose route: ",
               choices = c("All routes",
-                          sort(unique(
-                            munged_data[show_selection_based_on(selected_route_type(), route)]$route
-                          ))),
-              selected = "All routes")
-}); selected_route <- reactive({ input$route })
+                          sort(unique(munged_data$route[munged_data$route_type == input$route_type]
+                          ))))
+})
 
 output$lane_ui <- renderUI({
   selectInput("lane",
               "Choose lane: ",
               choices = c("All lanes",
-                          sort(unique(munged_data[show_selection_based_on(selected_route_type(), route_type) &
-                                                  show_selection_based_on(selected_route(), route)]$lane
-                          ))),
-selected = "All lanes")
-}); selected_lane <- reactive({ input$lane })
+                          sort(unique(munged_data$lane[munged_data$route_type == input$route_type &
+                                                         munged_data$route == input$route]
+                          ))))
+})
+
+final_selection <- reactive({
+  list(route_type_to_choose = input$route_type,
+       route_to_choose = input$route,
+       lane_to_choose = input$lane)
+})
 
 output$trafficPlot <- renderPlot({
-  print(
-    ggplot(munged_data[show_selection_based_on(selected_route_type(), item_to_test = route_type) &
-                         show_selection_based_on(selected_route(), item_to_test = route) &
-                         show_selection_based_on(selected_lane(), item_to_test = lane)],
-           aes(x = timestamp,
-               y = speed)) +
-      geom_point(alpha = 0.2) +
-      geom_smooth(method = "gam",
-                  formula = y ~ s(x, bs = "ps"),
-                  colour = "blue")
-  )
+  if (is.null(input$route_type) |
+        is.null(input$route) |
+        is.null(input$lane))
+    return()
+
+  isolate({
+    print(
+      ggplot(munged_data[show_selection_based_on(final_selection()$route_type_to_choose,
+                                                 item_to_test = munged_data$route_type) &
+                           show_selection_based_on(final_selection()$route_to_choose,
+                                                   item_to_test = munged_data$route) &
+                           show_selection_based_on(final_selection()$lane_to_choose,
+                                                   item_to_test = munged_data$lane)],
+             aes(x = timestamp,
+                 y = speed)) +
+        geom_point(alpha = 0.2) +
+        geom_smooth(method = "gam",
+                    formula = y ~ s(x, bs = "ps"),
+                    colour = "blue")
+    )
+  })
 })
