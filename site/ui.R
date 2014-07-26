1ada5d94fed4c9b9cc9213371bbe73a950c122f2
statwonk <cpeter9@gmail.com>
2014-07-25 22:58:03 -0400

    Define width.

--- site/ui.R
+++ site/ui.R
@@ -6,8 +6,8 @@ shinyUI(fluidPage(
       uiOutput("comparison_selections"),
       hr(),
       actionLink("add_another_comparison", "Add ..."),
-      actionLink("remove_another_comparison", "Remove ...")
-    ),
+      actionLink("remove_another_comparison", "Remove ..."),
+    width = 3),
     mainPanel(
       plotOutput("trafficPlot")
     )
