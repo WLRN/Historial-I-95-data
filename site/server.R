17c83adc2b9a6917337ae75b646380c20d4b4e6b
statwonk <cpeter9@gmail.com>
2014-08-03 12:31:03 -0400

    Let's restrict the scope a bit by looking at probabilies of sitting in a traffic jam, based on when you leave and which direction you're traveling.

--- site/server.R
+++ site/server.R
@@ -1,19 +1,9 @@
 require(shiny)
 require(eotr)
-require(rCharts)
 require(data.table)
 
-show_selection_based_on <- function(input_selection, item_to_test, comparison) {
-  if (grepl("All ", input_selection)) {
-    return(rep(comparison, length(item_to_test)))
-  } else {
-    return(ifelse(item_to_test %in% input_selection,
-                  comparison, NA))
-  }
-}
-
+# store_data(munge_data(read_data("sample.csv")))
+load("site_data.rda")
 shinyServer(function(session, input, output) {
-  load("site_data.rda")
-
   source("view.R", local = T)
 })
--- site/ui.R
+++ site/ui.R
@@ -1,19 +1,35 @@
-require(shiny); require(rCharts);
+require(shiny)
+# Inputs:
+# - day of week
+# - time of day
+# - direction
+#
+# Outputs:
+# - probability of slow (0-20), medium (20 - 50), fast (50+)
+
 shinyUI(fluidPage(
   titlePanel("End of the Road"),
   sidebarLayout(
     sidebarPanel(
-      uiOutput("comparison_selections"),
-      hr(),
-      actionLink("add_another_comparison", "Add ..."),
-      actionLink("remove_another_comparison", "Remove ..."),
-      width = 3),
+      selectInput("day_of_week", label = "Select day",
+                  choices = c("Sunday",
+                              "Monday",
+                              "Tuesday",
+                              "Wednesday",
+                              "Thursday",
+                              "Friday",
+                              "Saturday"),
+                  selected = "Monday"),
+      selectInput("time_of_day", label = "Hour of departure",
+                  choices = paste(c(12, 1:11), c(rep("AM", 12), rep("PM", 12))),
+                  selected = "8 AM"),
+      selectInput("direction", label = "Direction",
+                  choices = c("North", "South"),
+                  selected = "South")
+    ),
     mainPanel(
       fluidRow(
-        showOutput("speedPlot", "nvd3")
-      ),
-      fluidRow(
-        showOutput("volumeOccupancyPlot", "nvd3")
+        plotOutput("jam_probability")
       )
     )
   )
