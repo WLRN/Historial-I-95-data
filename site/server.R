cbee6dc36740dccb837760b92ad1d2382bd61499
statwonk <cpeter9@gmail.com>
2014-09-23 23:20:35 -0400

    My guess for posterity

--- site/server.R
+++ site/server.R
@@ -2,7 +2,6 @@ require(shiny)
 require(eotr)
 require(data.table)
 require(ggplot2); require(ggthemes); require(ggtern)
-rds <- connect_db()
 shinyServer(function(session, input, output) {
   source("ternary.R", local = T)
 })
